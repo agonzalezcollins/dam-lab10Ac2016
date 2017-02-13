626d1953abcc90601987c20ae7377fe65367c117
Agustin Gonzalez <agonzalezcollins@frsf.utn.edu.ar>
2017-02-13 19:29:47 -0300

    Archivo index.js, Arreglo de funcion eliminar tarea. TP Completo.

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -16,6 +16,9 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+ var contadorTareas=0;
+ var i=0;
+		
 var app = {
     // Application Constructor
     initialize: function() {
@@ -37,14 +40,49 @@ var app = {
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+        			
+		$("#btnAddTarea").click(
+			function(){
+				var str = "<a href=\"#\" class=\"list-group-item\" data-id-tarea=\""+contadorTareas+"\">"+"<h4 class=\"list-group-item-heading\">"+$("#tareaNombre").val()+"</h4>"+"<p class=\"list-group-item-text\"><div class=\"checkbox\"><label><input type=\"checkbox\">Finalizar</label></div></p></a>";
+				$("#listaTarea").append(str);
+				$("#tareaNombre").val('');
+				contadorTareas++;
+			}
+		);
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+		$("#btnVaciar").click( 
+			function(){ 
+				$("#listaTarea").empty(); 	
+			} 
+		);
 
-        console.log('Received Event: ' + id);
+		$("#listaTarea").on("click","a",function() {
+			var id_tarea = $(this).attr('data-id-tarea');
+			console.log("data-id-tarea="+id_tarea);
+			var posicion_tarea = 1 + $(this).index(); //index(): Vuelve la posición de base cero del elemento de la lista.
+			console.log("PosicionTarea="+posicion_tarea);
+
+            // Resultado del proceso de Dialogo (buttonIndex)
+              function onConfirm(buttonIndex){
+                  console.log("onConfirm= "+buttonIndex);
+                  console.log("ToRemove= "+id_tarea);
+                  if(buttonIndex == 1){ // Boton Si
+                      $("#listaTarea a:nth-child("+posicion_tarea+")").remove();
+                      contadorTareas--;
+                      alert('Se elimino tarea: '+(posicion_tarea));
+                  }
+              };
+
+			// Dialogo
+			navigator.notification.confirm(
+				'Desea eliminar?', // message
+				onConfirm,  // callback con el índice del botón presionado
+				'Eliminar Tarea',  // title
+				['Si' , 'Salir']  // etiquetas de los botones
+			);
+
+			return false; //Return Funcion, Importante para que no se ejecute dos veces.
+		});
     }
 };
 
--- www/js/index.js
+++ www/js/index.js
@@ -41,42 +41,48 @@ var app = {
     // Update DOM on a Received Event
     receivedEvent: function(id) {
         			
-		$("#btnAddTarea").click( 
-			function(){ 
-				var str = "<a href=\"#\" class=\"list-group-item\" data-id-tarea=\""+contadorTareas+"\">"+"<h4 class=\"list-group-item-heading\">"+$("#tareaNombre").val()+"</h4>"+"<p class=\"list-group-item-text\"><div class=\"checkbox\"><label><input type=\"checkbox\">Finalizar</label></div></p></a>";     
-				$("#listaTarea").append(str); 
-				$("#tareaNombre").val(''); 
-				contadorTareas++; 
-			}         
-		); 
+		$("#btnAddTarea").click(
+			function(){
+				var str = "<a href=\"#\" class=\"list-group-item\" data-id-tarea=\""+contadorTareas+"\">"+"<h4 class=\"list-group-item-heading\">"+$("#tareaNombre").val()+"</h4>"+"<p class=\"list-group-item-text\"><div class=\"checkbox\"><label><input type=\"checkbox\">Finalizar</label></div></p></a>";
+				$("#listaTarea").append(str);
+				$("#tareaNombre").val('');
+				contadorTareas++;
+			}
+		);
+
 		$("#btnVaciar").click( 
 			function(){ 
 				$("#listaTarea").empty(); 	
 			} 
 		);
-		
-		// Resultado del proceso de Dialogo
-		function onConfirm(buttonIndex) {
-			$("#listaTarea a:nth-child("+buttonIndex+")").remove();
-			contadorTareas--;	
-			alert('Se elimino: ' + buttonIndex);
-		 }
-		
-		$('#listaTarea').on("click","a",function() { 		
-			console.log($(this).attr('data-id-tarea')); 
-			var i = 1 + $(this).index(); 
-			console.log(i); 
-			
+
+		$("#listaTarea").on("click","a",function() {
+			var id_tarea = $(this).attr('data-id-tarea');
+			console.log("data-id-tarea="+id_tarea);
+			var posicion_tarea = 1 + $(this).index(); //index(): Vuelve la posición de base cero del elemento de la lista.
+			console.log("PosicionTarea="+posicion_tarea);
+
+            // Resultado del proceso de Dialogo (buttonIndex)
+              function onConfirm(buttonIndex){
+                  console.log("onConfirm= "+buttonIndex);
+                  console.log("ToRemove= "+id_tarea);
+                  if(buttonIndex == 1){ // Boton Si
+                      $("#listaTarea a:nth-child("+posicion_tarea+")").remove();
+                      contadorTareas--;
+                      alert('Se elimino tarea en posición: '+(posicion_tarea));
+                  }
+              };
+
+			// Dialogo
 			navigator.notification.confirm(
-				 'Desea eliminar?', // message
-				 onConfirm(i),  // callback con el índice del botón presionado
-				 'Eliminar Tarea',  // title
-				 ['Si','Salir']  // etiquetas de los botones
-			 );	
+				'Desea eliminar?', // message
+				onConfirm,  // callback con el índice del botón presionado
+				'Eliminar Tarea',  // title
+				['Si' , 'Salir']  // etiquetas de los botones
+			);
+
+			return false; //Return Funcion, Importante para que no se ejecute dos veces.
 		});
-		
-			
-					
     }
 };
 
